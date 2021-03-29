d0661a3328ea2ecf2269bb27818d7a06b3f39104
Kamil Jarosz <kjarosh256@gmail.com>
2021-03-29 23:32:45 +0200

    Add PVCs for Redis

--- src/main/java/com/github/kjarosh/agh/pp/k8s/K8sZone.java
+++ src/main/java/com/github/kjarosh/agh/pp/k8s/K8sZone.java
@@ -9,12 +9,21 @@ import io.kubernetes.client.openapi.models.V1ContainerBuilder;
 import io.kubernetes.client.openapi.models.V1Deployment;
 import io.kubernetes.client.openapi.models.V1DeploymentBuilder;
 import io.kubernetes.client.openapi.models.V1DeploymentSpec;
+import io.kubernetes.client.openapi.models.V1PersistentVolumeClaim;
+import io.kubernetes.client.openapi.models.V1PersistentVolumeClaimBuilder;
 import io.kubernetes.client.openapi.models.V1PodSpec;
 import io.kubernetes.client.openapi.models.V1PodTemplateSpec;
 import io.kubernetes.client.openapi.models.V1Service;
 import io.kubernetes.client.openapi.models.V1ServiceBuilder;
+import io.kubernetes.client.openapi.models.V1Volume;
+import io.kubernetes.client.openapi.models.V1VolumeBuilder;
+import io.kubernetes.client.openapi.models.V1VolumeMount;
+import io.kubernetes.client.openapi.models.V1VolumeMountBuilder;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -28,6 +37,7 @@ public class K8sZone {
     private final String namespace;
     private final String zoneId;
     private final String serviceName;
+    private final String pvcName;
     private final String deploymentName;
     private final Map<String, String> labels;
     private final String resourceCpu;
@@ -41,6 +51,7 @@ public class K8sZone {
         this.labels.put("zone", zoneId);
         this.deploymentName = zoneId;
         this.serviceName = zoneId;
+        this.pvcName = zoneId + "-pv";
         this.resourceCpu = resourceCpu;
         this.resourceMemory = resourceMemory;
     }
@@ -70,6 +81,7 @@ public class K8sZone {
                         .map(V1PodSpec::getContainers)
                         .map(c -> c.get(0))
                         .orElseGet(V1Container::new)))
+                .withVolumes(buildVolumes())
                 .endSpec()
                 .endTemplate()
                 .endSpec()
@@ -99,7 +111,24 @@ public class K8sZone {
                 .addToRequests("cpu", Quantity.fromString(resourceCpu))
                 .addToRequests("memory", Quantity.fromString(resourceMemory))
                 .endResources()
+                .withVolumeMounts(buildVolumeMounts())
                 .build();
+    }
+
+    private List<V1Volume> buildVolumes() {
+        return Collections.singletonList(new V1VolumeBuilder()
+                .withName(pvcName)
+                .editOrNewPersistentVolumeClaim()
+                .withClaimName(pvcName)
+                .endPersistentVolumeClaim()
+                .build());
+    }
+
+    private List<V1VolumeMount> buildVolumeMounts() {
+        return Collections.singletonList(new V1VolumeMountBuilder()
+                .withName(pvcName)
+                .withMountPath("/var/lib/redis")
+                .build());
     }
 
     private V1Service buildService(V1Service old) {
@@ -121,7 +150,25 @@ public class K8sZone {
                 .build();
     }
 
+    private V1PersistentVolumeClaim buildPvc(V1PersistentVolumeClaim old) {
+        Map<String, Quantity> requests = new HashMap<>();
+        requests.put("storage", Quantity.fromString("3Gi"));
+        return new V1PersistentVolumeClaimBuilder(old)
+                .editOrNewMetadata()
+                .withName(pvcName)
+                .endMetadata()
+                .editOrNewSpec()
+                .withNewStorageClassName("local-path")
+                .withAccessModes("ReadWriteOnce")
+                .editOrNewResources()
+                .withRequests(requests)
+                .endResources()
+                .endSpec()
+                .build();
+    }
+
     public void apply() throws ApiException {
+        applyPvc();
         applyDeployment();
         applyService();
     }
@@ -152,6 +199,19 @@ public class K8sZone {
         }
     }
 
+    private void applyPvc() throws ApiException {
+        try {
+            V1PersistentVolumeClaim oldPvc = coreApi.readNamespacedPersistentVolumeClaim(pvcName, namespace, null, false, false);
+            replacePvc(oldPvc);
+        } catch (ApiException e) {
+            if (e.getCode() != 404) {
+                throw e;
+            }
+
+            createPvc();
+        }
+    }
+
     public void createDeployment() throws ApiException {
         V1Deployment deployment = buildDeployment(new V1Deployment());
         appsApi.createNamespacedDeployment(namespace, deployment, null, null, null);
@@ -162,6 +222,11 @@ public class K8sZone {
         coreApi.createNamespacedService(namespace, service, null, null, null);
     }
 
+    public void createPvc() throws ApiException {
+        V1PersistentVolumeClaim pvc = buildPvc(new V1PersistentVolumeClaim());
+        coreApi.createNamespacedPersistentVolumeClaim(namespace, pvc, null, null, null);
+    }
+
     public void replaceDeployment(V1Deployment oldDeployment) throws ApiException {
         V1Deployment deployment = buildDeployment(oldDeployment);
         appsApi.replaceNamespacedDeployment(deploymentName, namespace, deployment, null, null, null);
@@ -170,5 +235,10 @@ public class K8sZone {
     public void replaceService(V1Service oldService) throws ApiException {
         V1Service service = buildService(oldService);
         coreApi.replaceNamespacedService(serviceName, namespace, service, null, null, null);
+    }
+
+    public void replacePvc(V1PersistentVolumeClaim oldPvc) throws ApiException {
+        V1PersistentVolumeClaim pvc = buildPvc(oldPvc);
+        coreApi.replaceNamespacedPersistentVolumeClaim(pvcName, namespace, pvc, null, null, null);
     }
 }
