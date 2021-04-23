dae2c866e32a044756700ff6adeb24d37a37cf6d
Adrian Wójciak <adrianwojciak@interia.pl>
2021-04-24 00:03:39 +0200

    [Deploy] ultimate bug fix
    (cherry picked from commit 89f9fb74e5bc669a702034ce9ae6361a0b7e8fe9)

--- src/main/java/com/github/kjarosh/agh/pp/cli/QueriesAveragesLoggerMain.java
+++ src/main/java/com/github/kjarosh/agh/pp/cli/QueriesAveragesLoggerMain.java
@@ -65,75 +65,99 @@ public class QueriesAveragesLoggerMain {
                 switch (next.getType()) {
                     case MEMBER:
                         if (next.getNaive()) {
-                            naiveMaxMembers.add(next.getMaxDuration());
-                            naiveAvgMembers.add(next.getAvgDuration());
+                            naiveMaxMembers.add(getDuration(getField(next.getMax(), "nano"), getField(next.getMax(), "seconds")));
+                            naiveAvgMembers.add(getDuration(getField(next.getAvg(), "nano"), getField(next.getAvg(), "seconds")));
                         } else {
-                            indexedMaxMembers.add(next.getMaxDuration());
-                            indexedAvgMembers.add(next.getAvgDuration());
+                            indexedMaxMembers.add(getDuration(getField(next.getMax(), "nano"), getField(next.getMax(), "seconds")));
+                            indexedAvgMembers.add(getDuration(getField(next.getAvg(), "nano"), getField(next.getAvg(), "seconds")));
                         }
                         break;
                     case REACHES:
                         if (next.getLabel() != null && next.getLabel().equals("exist")) {
                             if (next.getNaive()) {
-                                naiveMaxReachesExist.add(next.getMaxDuration());
-                                naiveAvgReachesExist.add(next.getAvgDuration());
+                                naiveMaxReachesExist.add(getDuration(getField(next.getMax(), "nano"), getField(next.getMax(), "seconds")));
+                                naiveAvgReachesExist.add(getDuration(getField(next.getAvg(), "nano"), getField(next.getAvg(), "seconds")));
                             } else {
-                                indexedMaxReachesExist.add(next.getMaxDuration());
-                                indexedAvgReachesExist.add(next.getAvgDuration());
+                                indexedMaxReachesExist.add(getDuration(getField(next.getMax(), "nano"), getField(next.getMax(), "seconds")));
+                                indexedAvgReachesExist.add(getDuration(getField(next.getAvg(), "nano"), getField(next.getAvg(), "seconds")));
                             }
                         } else {
                             if (next.getNaive()) {
-                                naiveMaxReachesNonexist.add(next.getMaxDuration());
-                                naiveAvgReachesNonexist.add(next.getAvgDuration());
+                                naiveMaxReachesNonexist.add(getDuration(getField(next.getMax(), "nano"), getField(next.getMax(), "seconds")));
+                                naiveAvgReachesNonexist.add(getDuration(getField(next.getAvg(), "nano"), getField(next.getAvg(), "seconds")));
                             } else {
-                                indexedMaxReachesNonexist.add(next.getMaxDuration());
-                                indexedAvgReachesNonexist.add(next.getAvgDuration());
+                                indexedMaxReachesNonexist.add(getDuration(getField(next.getMax(), "nano"), getField(next.getMax(), "seconds")));
+                                indexedAvgReachesNonexist.add(getDuration(getField(next.getAvg(), "nano"), getField(next.getAvg(), "seconds")));
                             }
                         }
                         break;
                     case EFFECTIVE_PERMISSIONS:
                         if (next.getNaive()) {
-                            naiveMaxEp.add(next.getMaxDuration());
-                            naiveAvgEp.add(next.getAvgDuration());
+                            naiveMaxEp.add(getDuration(getField(next.getMax(), "nano"), getField(next.getMax(), "seconds")));
+                            naiveAvgEp.add(getDuration(getField(next.getAvg(), "nano"), getField(next.getAvg(), "seconds")));
                         } else {
-                            indexedMaxEp.add(next.getMaxDuration());
-                            indexedAvgEp.add(next.getAvgDuration());
+                            indexedMaxEp.add(getDuration(getField(next.getMax(), "nano"), getField(next.getMax(), "seconds")));
+                            indexedAvgEp.add(getDuration(getField(next.getAvg(), "nano"), getField(next.getAvg(), "seconds")));
                         }
                         break;
                 }
             }
         } catch (NullPointerException error) {
             System.out.println("Type - MEMBERS:");
-            System.out.println("\tNaive:");
-            System.out.println("\t\tmax - " + (naiveMaxMembers.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveMaxMembers.size()));
-            System.out.println("\t\tavg - " + (naiveAvgMembers.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveAvgMembers.size()));
-            System.out.println("\tIndexed:");
-            System.out.println("\t\tmax - " + (indexedMaxMembers.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedMaxMembers.size()));
-            System.out.println("\t\tavg - " + (indexedAvgMembers.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedAvgMembers.size()));
+            if (naiveMaxMembers.size() > 0) {
+                System.out.println("\tNaive:");
+                System.out.println("\t\tmax - " + (naiveMaxMembers.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveMaxMembers.size()));
+                System.out.println("\t\tavg - " + (naiveAvgMembers.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveAvgMembers.size()));
+            }
+            if (indexedMaxMembers.size() > 0) {
+                System.out.println("\tIndexed:");
+                System.out.println("\t\tmax - " + (indexedMaxMembers.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedMaxMembers.size()));
+                System.out.println("\t\tavg - " + (indexedAvgMembers.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedAvgMembers.size()));
+            }
 
             System.out.println("Type - REACHES (existing):");
-            System.out.println("\tNaive:");
-            System.out.println("\t\tmax - " + (naiveMaxReachesExist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveMaxReachesExist.size()));
-            System.out.println("\t\tavg - " + (naiveAvgReachesExist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveAvgReachesExist.size()));
-            System.out.println("\tIndexed:");
-            System.out.println("\t\tmax - " + (indexedMaxReachesExist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedMaxReachesExist.size()));
-            System.out.println("\t\tavg - " + (indexedAvgReachesExist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedAvgReachesExist.size()));
+            if (naiveMaxReachesExist.size() > 0) {
+                System.out.println("\tNaive:");
+                System.out.println("\t\tmax - " + (naiveMaxReachesExist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveMaxReachesExist.size()));
+                System.out.println("\t\tavg - " + (naiveAvgReachesExist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveAvgReachesExist.size()));
+            }
+            if (indexedMaxReachesExist.size() > 0) {
+                System.out.println("\tIndexed:");
+                System.out.println("\t\tmax - " + (indexedMaxReachesExist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedMaxReachesExist.size()));
+                System.out.println("\t\tavg - " + (indexedAvgReachesExist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedAvgReachesExist.size()));
+            }
 
             System.out.println("Type - REACHES (nonexisting):");
-            System.out.println("\tNaive:");
-            System.out.println("\t\tmax - " + (naiveMaxReachesNonexist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveMaxReachesNonexist.size()));
-            System.out.println("\t\tavg - " + (naiveAvgReachesNonexist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveAvgReachesNonexist.size()));
-            System.out.println("\tIndexed:");
-            System.out.println("\t\tmax - " + (indexedMaxReachesNonexist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedMaxReachesNonexist.size()));
-            System.out.println("\t\tavg - " + (indexedAvgReachesNonexist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedAvgReachesNonexist.size()));
+            if (naiveMaxReachesNonexist.size() > 0) {
+                System.out.println("\tNaive:");
+                System.out.println("\t\tmax - " + (naiveMaxReachesNonexist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveMaxReachesNonexist.size()));
+                System.out.println("\t\tavg - " + (naiveAvgReachesNonexist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveAvgReachesNonexist.size()));
+            }
+            if (indexedMaxReachesNonexist.size() > 0) {
+                System.out.println("\tIndexed:");
+                System.out.println("\t\tmax - " + (indexedMaxReachesNonexist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedMaxReachesNonexist.size()));
+                System.out.println("\t\tavg - " + (indexedAvgReachesNonexist.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedAvgReachesNonexist.size()));
+            }
 
             System.out.println("Type - EFFECTIVE_PERMISSIONS:");
-            System.out.println("\tNaive:");
-            System.out.println("\t\tmax - " + (naiveMaxEp.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveMaxEp.size()));
-            System.out.println("\t\tavg - " + (naiveAvgEp.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveAvgEp.size()));
-            System.out.println("\tIndexed:");
-            System.out.println("\t\tmax - " + (indexedMaxEp.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedMaxEp.size()));
-            System.out.println("\t\tavg - " + (indexedAvgEp.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedAvgEp.size()));
+            if (naiveMaxEp.size() > 0) {
+                System.out.println("\tNaive:");
+                System.out.println("\t\tmax - " + (naiveMaxEp.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveMaxEp.size()));
+                System.out.println("\t\tavg - " + (naiveAvgEp.stream().reduce(Duration::plus).orElseThrow()).dividedBy(naiveAvgEp.size()));
+            }
+            if (indexedMaxEp.size() > 0) {
+                System.out.println("\tIndexed:");
+                System.out.println("\t\tmax - " + (indexedMaxEp.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedMaxEp.size()));
+                System.out.println("\t\tavg - " + (indexedAvgEp.stream().reduce(Duration::plus).orElseThrow()).dividedBy(indexedAvgEp.size()));
+            }
         }
+    }
+
+    private static long getField(Object obj, String field) {
+        return ((Integer) (((LinkedHashMap) obj).get(field))).longValue();
+    }
+
+    private static Duration getDuration(long nano, long seconds) {
+        return Duration.ofNanos(nano + seconds * 1000000000);
     }
 }
