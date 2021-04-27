cff60e9f86a33d968a10d70e6459e798c2e0b10d
Paweł Marszał <45012218+MrPaulMarshall@users.noreply.github.com>
2021-04-27 21:23:20 +0200

    Update get_report.sql
    (cherry picked from commit 03f77479d45262be9535e316eb542cea35b67276)

--- sql-and-python/get_report.sql
+++ sql-and-python/get_report.sql
@@ -1,5 +1,12 @@
 -- get_report.sql
+
+do $$ begin
+   perform report((select (min(time) + double precision '0.1' * (max(time) - min(time))) from dbnotification), (select max(time) from dbnotification));
+end $$
+
+-- old version - doesn't ignore launching period
+/*
 do $$ begin
    perform report((select min(time) from dbnotification), (select max(time) from dbnotification));
 end $$
-
+*/
