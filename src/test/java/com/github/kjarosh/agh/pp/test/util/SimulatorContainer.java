6f36e1e9d1b4d619ead4c2bc70cb41756ddc34b3
Kamil Jarosz <kjarosh256@gmail.com>
2021-06-01 00:59:37 +0200

    Add labels to GMM containers

--- src/test/java/com/github/kjarosh/agh/pp/test/util/SimulatorContainer.java
+++ src/test/java/com/github/kjarosh/agh/pp/test/util/SimulatorContainer.java
@@ -7,6 +7,7 @@ import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
 import java.nio.file.Paths;
+import java.time.Duration;
 
 /**
  * @author Kamil Jarosz
@@ -19,8 +20,10 @@ public class SimulatorContainer extends GenericContainer<SimulatorContainer> {
         this.withEnv("ZONE_ID", zoneId)
                 .withCommand("server")
                 .withNetworkAliases(zoneId)
+                .withLabel("gmm.zone", zoneId)
                 .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(zoneId)))
                 .waitingFor(new HttpWaitStrategy().forPath("/healthcheck"))
+                .withStartupTimeout(Duration.ofMinutes(5))
                 .withExposedPorts(80);
     }
 }
