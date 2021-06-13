3d2733df01168d3027945230ce8c4233a3765afa
Kamil Jarosz <kjarosh256@gmail.com>
2021-06-13 23:49:33 +0200

    Edge: toString

--- src/main/java/com/github/kjarosh/agh/pp/graph/model/Edge.java
+++ src/main/java/com/github/kjarosh/agh/pp/graph/model/Edge.java
@@ -12,33 +12,33 @@ import lombok.ToString;
 @ToString
 public class Edge implements Comparable<Edge> {
     @JsonProperty("src")
-    private final VertexId src;
+    private final VertexId from;
     @JsonProperty("dst")
-    private final VertexId dst;
+    private final VertexId to;
     @JsonProperty("perms")
     private final Permissions permissions;
 
     public Edge(
-            @JsonProperty("src") VertexId src,
-            @JsonProperty("dst") VertexId dst,
+            @JsonProperty("src") VertexId from,
+            @JsonProperty("dst") VertexId to,
             @JsonProperty("perms") Permissions permissions) {
-        this.src = src;
-        this.dst = dst;
+        this.from = from;
+        this.to = to;
         this.permissions = permissions != null ?
                 permissions : Permissions.NONE;
     }
 
     @JsonIgnore
     public EdgeId id() {
-        return new EdgeId(src, dst);
+        return new EdgeId(from, to);
     }
 
     public VertexId src() {
-        return src;
+        return from;
     }
 
     public VertexId dst() {
-        return dst;
+        return to;
     }
 
     public Permissions permissions() {
@@ -48,9 +48,9 @@ public class Edge implements Comparable<Edge> {
     @Override
     public int compareTo(Edge other) {
         int cmp;
-        cmp = src.compareTo(other.src);
+        cmp = from.compareTo(other.from);
         if (cmp != 0) return cmp;
-        cmp = dst.compareTo(other.dst);
+        cmp = to.compareTo(other.to);
         if (cmp != 0) return cmp;
 
         return permissions.compareTo(other.permissions);
